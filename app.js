a7630a36e854aca4503dadfef93ea71d67224b78
hanandadras <hanan.aabid@gmail.com>
2021-11-23 11:44:26 -0800

    changes to branch

--- app.js
+++ app.js
@@ -1,71 +1,71 @@
-// var commandLineArgs = process.argv;
-// console.log(commandLineArgs);
-// var profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
-// const message = 'Hello Node!';
-// message = 'Goodbye Node!';
+var commandLineArgs = process.argv;
+console.log(commandLineArgs);
+var profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+const message = 'Hello Node!';
+message = 'Goodbye Node!';
 
-// const sum = 5 + 3;
-// sum += 1;
+const sum = 5 + 3;
+sum += 1;
 
-// const animalArray = ['dog', 'cat', 'pig'];
+const animalArray = ['dog', 'cat', 'pig'];
 
-// animalArray.push('cow');
+animalArray.push('cow');
 
-// const personObj = {
-//   name: 'Lernantino',
-//   age: 99
-// };
+const personObj = {
+  name: 'Lernantino',
+  age: 99
+};
 
-// personObj.age = 100;
-// personObj.occupation = 'Developer';
-// var profileDataArgs = process.argv.slice(2, process.argv.length);
-// const printProfileData = (profileDataArr) => {
-//     console.log(profileDataArr);
-//   };
+personObj.age = 100;
+personObj.occupation = 'Developer';
+var profileDataArgs = process.argv.slice(2, process.argv.length);
+const printProfileData = (profileDataArr) => {
+    console.log(profileDataArr);
+  };
   
-//   printProfileData(profileDataArgs);
+  printProfileData(profileDataArgs);
 
-  // Notice the lack of parentheses around the `profileDataArr` parameter?
-// const printProfileData = profileDataArr => {
-//     for (let i = 0; i < profileDataArr.length; i++) {
-//       console.log(profileDataArr[i]);
-//     }
-//   };
-// let message = 'Hello Node!';
-// message = 'Hello ES6!';
+  Notice the lack of parentheses around the `profileDataArr` parameter?
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length; i++) {
+      console.log(profileDataArr[i]);
+    }
+  };
+let message = 'Hello Node!';
+message = 'Hello ES6!';
 
-// let sum = 5 + 3;
-// sum += 1;
+let sum = 5 + 3;
+sum += 1;
 
-// const message = 'Hello Node!';
+const message = 'Hello Node!';
 
-// if (true === true) {
-//   const message = 'Hello ES6!';
-//   let sum = 5;
-//   sum += 10;
-//   console.log(message);
-//   console.log(sum);
-// }
+if (true === true) {
+  const message = 'Hello ES6!';
+  let sum = 5;
+  sum += 10;
+  console.log(message);
+  console.log(sum);
+}
 
-// console.log(message);
-// console.log(sum);
+console.log(message);
+console.log(sum);
 
-// const printProfileData = profileDataArr => {
-//    /* this is the same as*/
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//       console.log(profileDataArr[i]);
-//     }
+const printProfileData = profileDataArr => {
+   /* this is the same as*/
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+      console.log(profileDataArr[i]);
+    }
   
-//     console.log('================');
+    console.log('================');
   
-//    /*this*/
-//     profileDataArr.forEach((profileItem) => {
-//       console.log(profileItem)
-//     });
-//   };
+   /*this*/
+    profileDataArr.forEach((profileItem) => {
+      console.log(profileItem)
+    });
+  };
 
 const printProfileData = profileDataArr;
 profileDataArr.forEach(profileItem => console.log(profileItem));
